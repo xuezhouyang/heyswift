72cc9a9b3a8cd6bbb6e49f734e60ad682170224b
xuezhouyang <xuezhouyang@xuezhouangsimac.lan>
2015-12-20 16:34:36 +0800

    练习xcode使用

---
+++ hey/hey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hey
+//
+//  Created by xuezhouyang on 15/12/20.
+//  Copyright © 2015年 xuezhouyang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
