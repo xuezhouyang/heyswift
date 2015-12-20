ef812abf82c463e062c0b1bf74c9b999df2c35c1
xuezhouyang <xuezhouyang@xuezhouangsimac.lan>
2015-12-21 00:21:36 +0800

    Test

---
+++ hey/hey/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+var str:String = "Hello My QWERQWERQWERQWEREDFGQERG3Q5GPlayground"
+str = "asdfasdfasdf"
+let CONST = "QWEERTGQ1235G354GRQEWRQWERQWERasdflkjDEFQWERFQ24FQ2FFDQWFasdf"
+print(CONST);
+
+var sampleInteger:Int;
+var temInteger = 12;
+
+var temIntegers = Int.init(12);
+temIntegers.bigEndian;
